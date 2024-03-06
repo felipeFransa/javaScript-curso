a0e5b293d6878a072b6ea76163273db10b7cb462
Felipe <antoniofelipesilva@live.com>
2024-03-06 15:34:06 -0300

    aula 1

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Bem vindo!!!")
+
+
+let Car = {
+    marca: "ford",
+    modelo: "focus",
+    ano: 2020,
+    velocidade: 100,
+    acelerar: function(){
+        console.log("De 0 a 100 em 10.8s")
+    }
+}
+
+console.log(Car)
+
+let Times = [
+    { name: "Roma"},
+    { name: "Milan"},
+    { name: "Napoli"},
+    { name: "juve"}
+]
+
+console.log(Times)
